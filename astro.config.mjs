0a95a094592dbf8f6bb3fe58a4fe2e7d75faa54f
Tomislav Peric <5732832+tperich@users.noreply.github.com>
2026-03-18 16:05:35 +0100

    Add ENV and types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv(process.env.NODE_ENV ?? 'production', process.cwd(), '');
+
+if (!env.SITE_URL) {
+    throw new Error('Missing SITE_URL environment variable. Please set it in a .env file or as an environment variable.');
+}
+
+export default defineConfig({
+    site: env.SITE_URL,
+    base: env.SITE_BASE || '/',
+});
